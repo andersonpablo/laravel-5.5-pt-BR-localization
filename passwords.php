6a39fb0bfb0d8955e969584dc67d0f67d98a2dce
andersonpablo <mib_anderson@hotmail.com>
2020-03-03 11:41:20 -0300

    Update passwords.php

--- passwords.php
+++ passwords.php
@@ -17,6 +17,7 @@ return [
     'reset' => 'Sua senha foi redefinida!',
     'sent' => 'Enviamos seu link de redefinição de senha por e-mail!',
     'token' => 'Este token de redefinição de senha é inválido.',
-    'user' => "Não encontramos um usuário com esse endereço de e-mail.",
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'user' => 'Não encontramos um usuário com esse endereço de e-mail.',
 
 ];
